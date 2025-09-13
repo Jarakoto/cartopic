91aebd6ba4010ae4bbe83a5209b0923f0af1f213
Johary Rakotomalala <johary.rakotomalala.31@gmail.com>
2025-09-13 15:47:53 +0200

    Remove pycache add trips and Steps

--- backend/trips/admin.py
+++ backend/trips/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Trip, Step
+
+class StepInline(admin.TabularInline):
+	model = Step
+	extra = 1
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+	inlines = [StepInline]
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'trip')
